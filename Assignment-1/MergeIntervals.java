0dfd9e6b3d8139d4a0d7fc89e9fd5f51513def81
winmoee <winmoe@berkeley.edu>
2023-02-28 17:06:21 -0800

    mergeintervals cannot get

--- Assignment-1/MergeIntervals.java
+++ Assignment-1/MergeIntervals.java
@@ -1,11 +1,11 @@
+import org.antlr.v4.runtime.tree.Tree;
 import org.junit.Assert;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MergeIntervals {
-    // Two arrays/strings increment/decrement hashmap counts
+    // sort then solve.
+
     /*
     Time complexity:
     Space complexity:
@@ -17,9 +17,39 @@ public class MergeIntervals {
 
     // input List, output List,
     // how will it be represented?
-    // could be treemap since it's ordered...
+    // use list of maps with only one key value pair each.
+    // in python it would be tuple, but i don't know any java tuples.
 
-    public static List<Integer> mergeIntervals(List) {
+    // sort the treemaps by key value pair.
+
+    public static List<Map<Integer, Integer>> mergeIntervals(List<Map<Integer, Integer>> input) {
+        //getting sorted
+        List<Integer> toSort = new ArrayList<>();
+
+        for (Map<Integer, Integer> map : input) {
+            for (int i : map.keySet()) {
+                toSort.add(i);
+            }
+        }
+
+        Collections.sort(toSort);
+
+        //now based on that sorted keyset, we will sort the entire list based on keys
+        List<Map<Integer, Integer>> inputSorted = new ArrayList<>();
+
+        for (int i : toSort) {
+            for (int j = 0; j < input.size(); j++) {
+                if (input.get(j).containsKey(i)) {
+                    inputSorted.add(input.get(j));
+                }
+            }
+        }
+
+        return inputSorted;
+                //List<Map<Integer, Integer>>
+
+        Input: [(2, 3), (4, 8), (1, 2), (5, 7), (9, 12)]
+        Output: [(4, 8), (1, 3), (9, 12)]
 
     }
     public static void main(String[] args) {
